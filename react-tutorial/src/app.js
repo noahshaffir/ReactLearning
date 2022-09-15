7adc547a5d94051a1c0e4816225a919b28f32107
noahshaffir <noahshaffir@gmail.com>
2022-09-15 16:57:17 -0400

    currently listening to flashing lights by kanye west

--- react-tutorial/src/app.js
+++ react-tutorial/src/app.js
@@ -14,9 +14,27 @@ class App extends Component {
 */
 class App extends Component {
   render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
     return (
       <div className='container'>
-        <Tablee />
+        <Tablee characterData={characters} />
       </div>
     )
   }
--- react-tutorial/src/tablee.js
+++ react-tutorial/src/tablee.js
@@ -9,34 +9,25 @@ const TableHeader = () => {
       </thead>
     )
   }
-  const TableBody = () => {
-    return (
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
+  const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
         </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
-    )
+      )
+    })
+  
+    return <tbody>{rows}</tbody>
   }
   class Tablee extends Component {
-    render() {
+    render() { 
+        const { characterData } = this.props
       return (
         <table>
           <TableHeader />
-          <TableBody />
+          <TableBody characterData = {characterData} />
         </table>
       )
     }
